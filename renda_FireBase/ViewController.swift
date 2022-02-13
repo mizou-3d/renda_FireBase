b4bf39970b06ad6834887241b9a29dc477ef9a7e
SayakaMizote <mizou.3d@gmail.com>
2022-02-13 16:44:25 +0900

    Initial Commit

---
+++ renda_FireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  renda_FireBase
+//
+//  Created by 溝手彩加 on 2022/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
